8fcd899093a7f40f550e640a3d14e67f5e576512
Emese Mathe <emese.mathe@student.upt.ro>
2018-11-05 17:13:50 +0200

    replaced Stack with ArrayBlockingQueue in QUEUE class AND implemented isValid in TOPIC class

--- src/messenger/Queue.java
+++ src/messenger/Queue.java
@@ -1,20 +1,32 @@
-package messenger;
+	package messenger;
 
-import java.util.Stack;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Queue extends Resource {
 	private int maxNumberOfMessages;
-	private Stack<Message> messages;
+	
+	// ArrayBlockingQueue is good when multiple threads are used
+	private ArrayBlockingQueue<Message> messages;
 	
 	public boolean add(Message m) {
 		if (messages.size() < maxNumberOfMessages && m.getRecipient() != null) {
-			messages.push(m);
+			try {
+			messages.put(m);
 			return true;
+			}
+			catch(NullPointerException e)
+			{
+				System.out.println("Null value!");
+				return false;
+			} catch (InterruptedException e) {
+				System.out.println("Waiting for stack to have space!");
+				return false;
+			}
 		}
 		else return false;
 	}
 	public Message remove(Message m) {
-		return messages.pop();
+		return messages.remove();
 	}
 	
 	@Override
--- src/messenger/Topic.java
+++ src/messenger/Topic.java
@@ -3,22 +3,22 @@ package messenger;
 public class Topic extends Resource {
 	private TopicType topic;
 	private Message message;
-	private float timeOut;
+	private int timeOut;
 	
 	public TopicType getTopicType() {
 		return topic;
 	}
-	public Topic(Message m, TopicType t) {
+	public Topic(Message m, TopicType t, int time) {
 		if(m.getRecipient() == null)
 			message = m;
 		topic = t;
-		
+		timeOut = time;
 	}
 	
 	//to be implemented
-	public boolean isValid()
+	public boolean isValid(int time)
 	{
-		return true;
+		return timeOut < time;
 	}
 	
 	public Message getMessage()
